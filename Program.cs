e6ed467685cba15d2ed0b21e474e0ed17370dc5a
Jeremy <jveto91@gmail.com>
2018-10-08 14:38:49 -0500

    hey2

--- Program.cs
+++ Program.cs
@@ -19,10 +19,7 @@ namespace TrainTracker
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
-                .UseIISIntegration()
                 .Build();
     }
 }
